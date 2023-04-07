680fe994bed81882051e059ed17cee316f217ac0
Dooober <cooperdean2002@gmail.com>
2023-04-07 14:40:27 -0400

    defaults folder

--- src/main/java/edu/wpi/teamname/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamname/controllers/HomeController.java
@@ -1,9 +1,23 @@
 package edu.wpi.teamname.controllers;
 
+import com.sun.javafx.collections.ImmutableObservableList;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXFilterComboBox;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
 public class HomeController {
 
   @FXML
-  public void initialize() {}
+  MFXComboBox<String> comboBox;
+
+  @FXML
+  MFXFilterComboBox<String> filterComboBox;
+
+  @FXML
+  public void initialize() {
+    ObservableList<String> list = new ImmutableObservableList<>("one", "two", "three", "four");
+    comboBox.setItems(list);
+    filterComboBox.setItems(list);
+  }
 }
